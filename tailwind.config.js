f7a5f4e2622299fcf083007abb25b5ca07f1be5b
Nkaka23dev <nkakaeric96@gmail.com>
2022-10-13 12:30:21 +0200

    BEM changes was made on HOME page(Implementing comments from Yannick)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-       lightBlack:'#1D2130'
+       lightBlack:'#1D2130',
       },
       fontFamily: {
-        robotoFont: ['Roboto']
+        robotoFont: ['Roboto'],
+      },
+      background:{
+        custbg1: 'radial-gradient(483.9% 2719.65% at -49.5% -250%, #D798E1 17.55%, #9BFFA5 27.56%, #AED3FF 49.89%, #C9D4EF 56.53%, #CACFFA 65.69%);',
+        custbg3: 'radial-gradient(483.9% 2719.65% at -49.5% -250%, #d798e173 17.55%, #9bffa563 27.56%, #AED3FF 49.89%, #C9D4EF 56.53%, #CACFFA 65.69%)',
+        custbg2: 'rgba(215, 152, 225, 0.15)'
       }
     },
   },
